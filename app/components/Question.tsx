8e8ffffbbda547633b2b483bc0cf9ce22415a6ff
Lojnik <loj210680@spengergasse.at>
2024-11-18 09:40:39 +0100

    DEUTSCH MACHEN -fd Johnny -fd Walter

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -36,14 +36,14 @@ export default function Question() {
 
       <div className="mb-4">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Audior Comment
+          Audior Kommentar
         </label>
         <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
       </div>
       {selectedStatus === "2" || selectedStatus === "3" ? (
       <div className="mb-4">
         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Finding Comment
+          Finding Kommentar
         </label>
         <textarea id="message" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
       </div>
