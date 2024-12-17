a2f5e975670fed20b8236585690d877ead6d2547
KristianPopovic <pop210687@spengergasse.at>
2024-12-17 08:53:48 +0100

    Frontend connected with API Gruppe-5

--- frontend/app/routes/gruppe5.tsx
+++ frontend/app/routes/gruppe5.tsx
@@ -1,26 +1,59 @@
-export default function setup() {
-  main();
+import { useEffect, useState } from 'react';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Navbar } from "~/components/Navbar";
+
+export default function Setup() {
+  const [findings, setFindings] = useState([]);
+
+  useEffect(() => {
+    async function fetchFindings() {
+      const response = await showAllFindings();
+      if (response.status == 404) {
+        console.log("404 - Not Found");
+      } else {
+        const data = await response.json();
+        setFindings(data);
+      }
+    }
+    fetchFindings();
+  }, []);
+
   return (
-    <div className="350px px-10 mt-3">
-      <h1 className="text-2xl text-bold">Findings</h1>
+    <div>
+      <Navbar />
+      <div className="max-w-350px px-10 mt-3">
+        <h1 className="text-2xl font-bold">Findings</h1>
+        <ul>
+          {findings.length > 0 ? (
+            findings.map((finding) => (
+              <Card className="w-[350px]">
+                <CardHeader className="m-2 text-2xl">
+                  <CardTitle><p><strong>ID:</strong> {finding.f_id}</p></CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p><strong>Erstelldatum:</strong> {finding.f_creation_date}</p>
+                  <p><strong>Status:</strong> {finding.f_status}</p>
+                </CardContent>
+              </Card>
+            ))
+          ) : (
+            <p>Loading or no findings available</p>
+          )}
+        </ul>
+      </div >
     </div>
   );
 }
 
-export async function main() {
-  var response = await showAllFindings();
-  if (response.status == 404) {
-    console.log("404");
-  }
-  else {
-    console.log("Hier");
-  }
-  console.log(response);
-}
-
 export async function showAllFindings() {
-  var response = await fetch('http://localhost:3000/findings/getall', {
-    method: 'POST',
+  const response = await fetch('http://localhost:3000/findings/getall', {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
